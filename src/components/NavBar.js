c2d233c1189a5e3bfa3f976089228f0c30475c2b
Lewis <lewisdsykes@gmail.com>
2022-08-12 14:50:50 +0100

    add NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,16 +7,34 @@ const NavBar = () => {
     return(
 
         <>
-        
-        
+            <Nav>
+                <List>
+                    <Items>Home</Items>
+                    <Items>Projects</Items>
+                    <Items>About Me</Items>
+                </List>
+            </Nav>
         
         
         </>
-
-
-
     );
 }
 
 
 export default NavBar;
+
+const Nav = styled.div`
+display: flex;
+list-style: none;
+justify-content: flex-start;
+background-color: grey;
+`
+
+const List = styled.ul`
+display: flex;
+`
+
+const Items = styled.a`
+list-style: none;
+padding-right: 20px;
+`
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PortfolioTitle from "../components/Title";
+import NavBar from "../components/NavBar";
 
 
 const PortfolioContainter = () => {
@@ -8,7 +9,7 @@ const PortfolioContainter = () => {
     return (
 
         <>
-
+            <NavBar/>
            <PortfolioTitle/>
             
         </>
