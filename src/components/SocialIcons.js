a73ff6fcb03be75cd56a982ad648cf8f4d1680e4
Lewis <lewisdsykes@gmail.com>
2022-08-14 18:22:02 +0100

    added cv button

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -11,6 +11,7 @@ const SocialIcons = () => {
 
 
             <Icons>
+                <CV type="button">CV</CV>
                 <a href="https://www.instagram.com/lewissykes95/"><Items src="https://cdn-icons-png.flaticon.com/512/87/87390.png"/></a>
                 <a href="https://www.linkedin.com/in/lewis-sykes-b6979b115/"><Items src="https://cdn-icons-png.flaticon.com/512/25/25320.png"/></a>
             </Icons>
@@ -36,6 +37,24 @@ margin: 10px;
 width: 30px;
 &:hover {
     transform: scale(1.1);
+    cursor: pointer;
+  }
+
+`
+
+const CV = styled.button`
+position: relative;
+bottom: 260px;
+left: 80px;
+font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+color: purple;
+width: 100px;
+height: 30px;
+border-radius: 15px;
+border: 1px purple solid;
+&:hover {
+    background-color: purple;
+    color: white;
     cursor: pointer;
   }
 
