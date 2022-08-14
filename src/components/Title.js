64ed4a3a15108d8faebe42a431404d45066b499e
Lewis <lewisdsykes@gmail.com>
2022-08-14 11:20:47 +0100

    added languages

--- src/components/Title.js
+++ src/components/Title.js
@@ -10,8 +10,18 @@ const PortfolioTitle = () => {
         <>
         
             <Title>Lewis Sykes Portfolio</Title>
-        
-        
+            <About>I am a Junior Software Developer from Edinburgh</About>
+            <Skills>
+                <Items>Python</Items>
+                <Items>JavaScript</Items>
+                <Items>HTML + CSS</Items>
+            </Skills>
+
+            <Frameworks>
+                <Items>Flask</Items>
+                <Items>React</Items>
+            </Frameworks>
+            
         </>
 
 
@@ -25,4 +35,18 @@ export default PortfolioTitle;
 const Title = styled.h1`
 font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
 
+`
+
+const About = styled.h3`
+font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+`
+
+const Skills = styled.div``
+
+
+const Frameworks = styled.div``
+
+
+const Items = styled.h4`
+font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
 `
